7a393a027c69b7c5d382332fc3d4bfc5ea21603d
bryan <bryan@plato.smittex.net>
2013-09-13 13:02:03 -0500

    hello

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/bryan/dev/pg-tests/platforms/android/bin/AndroidManifest.xml : \
+/home/bryan/dev/pg-tests/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/home/bryan/dev/pg-tests/platforms/android/bin/classes.dex : \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R$string.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/HelloWorld.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R$xml.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R$layout.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R$attr.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/R$drawable.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/classes/com/example/hello/BuildConfig.class \
+/home/bryan/dev/pg-tests/platforms/android/bin/dexedLibs/cordova-3.0.0-b8afe9530611588d2f9ecc50828a407a.jar \
