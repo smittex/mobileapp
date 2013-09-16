fdecde46b0070b8da0c23bd45608ef732cd1028e
bryan <bryan@scarlett.(none)>
2013-09-16 16:06:31 -0500

    add intelliJ files

--- out/production/android/script.js
+++ out/production/android/script.js
@@ -10,9 +10,6 @@ var app = {
 	    	// Stub for when device goes offline
 	    }
 	},
-    dal: {
-        // Stub for DAL CRUD ops
-    },
 	screens: [
 		'first',
 		'category-intro',
@@ -21,9 +18,7 @@ var app = {
 		'question-container',
 		'product-page'
 	],
-    history: {},
     initialize: function() {
-        this.renderTemplates();
         this.bindEvents();
     },
     bindEvents: function() {
@@ -36,52 +31,7 @@ var app = {
         console.log('Received Event: ' + id);
     },
     onNext: function() {
-        var screen = $(this).parents('.screen').data('screen');
-    	console.log('Moving away from screen \'' + screen + '\'');
-
-        /* Jessie's code */
-        //Enable some links to move the app to the next screen and hide all screens but the current one
-        var oldSlide = $(this).parents('.screen');
-        var newSlide = $(oldSlide).next('.screen');
-
-        $(newSlide).show();
-        $(newSlide).addClass('current');
-
-        $(oldSlide).removeClass('current');
-        $(oldSlide).animate({
-            left:'-10%'
-        }, 250, 'cubic-bezier(0, 0, 0.20, 1)', function() {
-            $(oldSlide).hide();
-        });
-        $(newSlide).animate({
-            left:'0%'
-        }, 250, 'cubic-bezier(0, 0, 0.20, 1)');
-        /* End Jessie's code */
-    },
-    compileTemplate: function() {
-        // Stub for compiling templates
-        // Should we precompile the templates so we don't have to do it at runtime?
-    },
-    renderTemplate: function() {
-        // Stub for rendering data in a template
-    },
-    renderTemplates: function() {
-        if (!cat_intro_tmpl) {
-            // Introduction to Sound Detection, Protection, & Validation
-            var cat_intro_ctx = {title:'Sound Detection',header:'Getting Started',
-                description:'<p>Complying with regulations, selecting appropriate protection ' +
-                    'and analyzing noise control options are all easier with 3M ' +
-                    'detection Solutions instrumentation for exposure assessment, noise ' +
-                    'analysis and creating a hearing conservation program.</p>' +
-                    '<p>This guide will help you select the right instrumentation for ' +
-                    'your assessment needs with confidence. Choose your application to begin.</p>' +
-                    '<p><strong>Questions? Contact your 3M Detection Solutions ' +
-                    'representative or 3M Technical Service at 800-245-0779.</strong></p>'};
-            var cat_intro_tmpl = Handlebars.compile($('#intro-tmpl').html());
-            var cat_intro_html = cat_intro_tmpl(cat_intro_ctx);
-
-            $('[data-screen=category-intro]').html(cat_intro_html);
-        }
+    	console.log(this.closest(app.screens));
     }
 };
 
@@ -97,7 +47,22 @@ $(function(){
 	var otherSlides = $('.first').siblings('.screen');
 	$(otherSlides).hide();
 
+    if (!cat_intro_tmpl) {
+        // Introduction to Sound Detection, Protection, & Validation
+        var cat_intro_ctx = {title:'Sound Detection',header:'Getting Started',
+            description:'<p>Complying with regulations, selecting appropriate protection ' +
+            'and analyzing noise control options are all easier with 3M ' +
+            'detection Solutions instrumentation for exposure assessment, noise ' +
+            'analysis and creating a hearing conservation program.</p>' +
+            '<p>This guide will help you select the right instrumentation for ' +
+            'your assessment needs with confidence. Choose your application to begin.</p>' +
+            '<p><strong>Questions? Contact your 3M Detection Solutions ' +
+            'representative or 3M Technical Service at 800-245-0779.</strong></p>'};
+        var cat_intro_tmpl = Handlebars.compile($('#intro-tmpl').html());
+        var cat_intro_html = cat_intro_tmpl(cat_intro_ctx);
 
+        $('.category-intro').html(cat_intro_html);
+    }
 
     if (!cat_ass_tmpl) {
         // Category's assessments
@@ -110,7 +75,7 @@ $(function(){
         var cat_ass_tmpl = Handlebars.compile($('#assessment-template').html());
         var cat_ass_html = cat_ass_tmpl(cat_ass_ctx);
 
-        $('[data-screen=select-assess]').append(cat_ass_html);
+        $('.select-assess').append(cat_ass_html);
     }
 
     if (!ass_intro_tmpl) {
@@ -123,7 +88,7 @@ $(function(){
         var ass_intro_tmpl = Handlebars.compile($('#assessment-intro-tmpl').html());
         var ass_intro_html = ass_intro_tmpl(ass_intro_ctx);
 
-        $('[data-screen=assessment-intro]').html(ass_intro_html);
+        $('.assessment-intro').html(ass_intro_html);
     }
 
     if (!qtn_tmpl) {
@@ -134,7 +99,7 @@ $(function(){
         var qtn_tmpl = Handlebars.compile($('#question-tmpl').html());
         var qtn_html = qtn_tmpl(qtn_ctx);
 
-        $('[data-screen=question-container]').html(qtn_html);
+        $('.question-container').html(qtn_html);
     }
 
     if (!prod_tmpl) {
@@ -148,7 +113,7 @@ $(function(){
         var prod_tmpl = Handlebars.compile($('#product_template').html());
         var prod_html = prod_tmpl(prod_ctx);
 
-        $('[data-screen=product-page]').html(prod_html);
+        $('product-page').html(prod_html);
     }
 
 
@@ -156,14 +121,41 @@ $(function(){
 
 
 	
-    $('a.det-icon, a.pro-icon, a.com-icon').click(function(){
-        $('nav.main-nav').show();
+$('a.det-icon, a.pro-icon, a.com-icon').click(function(){
+
+			$('nav.main-nav').show();
+				
 	});
 	
 	$('a.main-page').click(function(){
 		$('nav.main-nav').hide();
+	
 	});
+	
 
+//Enable some links to move the app to the next screen and hide all screens but the current one		
+	$('a.next').click(function(){
+		var oldSlide = $(this).parents('.screen');
+		var newSlide = $(oldSlide).next('.screen');
+		$(newSlide).show();
+		
+
+		
+		
+		
+		
+		$(newSlide).addClass('current');
+		$(oldSlide).removeClass('current');
+		$(oldSlide).animate({
+			left:'-10%'
+		}, 250, 'cubic-bezier(0, 0, 0.20, 1)', function() {
+			$(oldSlide).hide();
+			});
+		$(newSlide).animate({
+			left:'0%'
+		}, 250, 'cubic-bezier(0, 0, 0.20, 1)');
+
+	});
 
 //Enable some links to move the app back one screen and hide all screens but the current one
 	$('a.back').click(function(){
